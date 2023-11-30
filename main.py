f6ebad4c7d7cb9c80ffde9f5f3b49d3044a7fe5a
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2023-11-30 16:42:42 +0100

    fin

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from PyQt5 import QtWidgets
 from controladores.controladorLogin import Controlador_login
 import sys
+from CA.CAR.CAR import CAR
 
 
 if __name__ == '__main__':                      #  Iniciador del código
     app = QtWidgets.QApplication(sys.argv)
     login = Controlador_login()
     login.show()
+    ca = CAR()
     sys.exit(app.exec_())
